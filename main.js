683a6e9201745acf6016e3a2cb424effe4de2a45
Kamil Markiewicz <Kamil.Markiewicz1997@gmail.com>
2026-02-24 13:46:15 +0000

    Electron Integration
    Initial setup and example for running elecrtron app.

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Electron launched.')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    console.log("Closing application.")
+    app.quit()
+  }
+})
