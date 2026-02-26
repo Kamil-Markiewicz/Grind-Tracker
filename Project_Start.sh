5e5a014f63062551b37f9459bcfad2e6ca8e98b5
Kamil Markiewicz <Kamil.Markiewicz1997@gmail.com>
2026-02-26 14:01:21 +0000

    Electron-forge packaging example

--- Project_Start.sh
+++ Project_Start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+#Install Electron
 npm install electron --save-dev
+
+#Install Electron-forge
+npm install --save-dev @electron-forge/cli
+npx electron-forge import
+sudo pacman -S dpkg, fakeroot
